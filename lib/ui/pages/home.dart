98c344249ecafce3a8b5362d50da6f0749b5c206
Burak ŞEKER <xburakseker@gmail.com>
2022-11-24 12:09:47 +0300

    ss

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -29,7 +29,7 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     // TODO: implement initState
-    Albums().getArtistData();
+    Provider.of<AlbumsViewModel>(context).getArtistWithId();
     super.initState();
   }
 
